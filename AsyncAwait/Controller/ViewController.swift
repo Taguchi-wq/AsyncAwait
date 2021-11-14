d606e5ea11a02b16759c99d2a3a1f515fa1233c8
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-15 03:20:09 +0900

    tableViewwo

--- AsyncAwait/Controller/ViewController.swift
+++ AsyncAwait/Controller/ViewController.swift
@@ -8,11 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    private var users: [User] = []
+    
+    
+    // MARK: - @IBOutlets
+    @IBOutlet private weak var tableView: UITableView!
+    
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableView(tableView)
+    }
+    
+    
+    // MARK: - Methods
+    private func setupTableView(_ tableView: UITableView) {
+        tableView.dataSource = self
     }
 
 
 }
 
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = users[indexPath.row].name
+        return cell
+    }
+    
+}
